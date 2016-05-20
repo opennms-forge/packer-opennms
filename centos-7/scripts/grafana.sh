6c2aeae710005526279e5fb1362330d0248d8669
Ronny Trommer <ronny@opennms.org>
2016-05-20 21:38:23 +0200

    Fix installing grafana package with yum

--- centos-7/scripts/grafana.sh
+++ centos-7/scripts/grafana.sh
@@ -1,8 +1,9 @@
-sudo yum install https://grafanarel.s3.amazonaws.com/builds/grafana-3.0.2-1463383025.x86_64.rpm
+yum -y install https://grafanarel.s3.amazonaws.com/builds/grafana-3.0.2-1463383025.x86_64.rpm
 systemctl daemon-reload
-systemctl enable grafana-server
-systemctl start grafana-server
+systemctl enable grafana-server.service
+systemctl start grafana-server.service
 firewall-cmd --zone=public --permanent --add-port=3000/tcp
 sleep 5
 grafana-cli plugins install opennms-datasource
 curl 'http://admin:admin@localhost:3000/api/datasources' -X POST -H 'Content-Type:application/json;charset=UTF-8' --data-binary '{"name":"OpenNMS","type":"OpenNMS","access":"proxy","url":"http://localhost:8980/opennms","isDefault":true,"basicAuth":true,"basicAuthUser":"admin","basicAuthPassword":"admin"}'
+systemctl restart grafana-server.service
