321c6670cbd0bb0c080a44c8a980e4c22472c5ff
Ronny Trommer <ronny@opennms.org>
2017-02-24 14:57:10 +0100

    Drop Ubuntu 16.04 support

--- ubuntu-lts/scripts/networking.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash -eux
-
-# Cleaning up udev and dhcp leases
-rm -rf /dev/.udev/ /var/lib/dhcp3/* /var/lib/dhcp/*;
--- ubuntu-lts/scripts/tools.sh
+++
@@ -1,1 +0,0 @@
-apt-get install -y vim-nox curl htop iftop sysstat tcpdump
